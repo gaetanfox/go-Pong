f61294265e7beade068733f43e542cc2bcb8143e
Gaetan Fox <bgloup@gmail.com>
2019-10-26 12:41:42 +0200

    Working rough prototype

--- pong.go
+++ pong.go
@@ -15,6 +15,8 @@ type pos struct {
 	x, y float32
 }
 
+//BALL STUFF
+
 type ball struct {
 	pos
 	radius int
@@ -34,6 +36,32 @@ func (ball *ball) draw(pixels []byte) {
 	}
 }
 
+//Move the ball
+func (ball *ball) update(leftPaddle *paddle, rightPaddle *paddle) {
+	ball.x += ball.xv
+	ball.y += ball.yv
+
+	if int(ball.y)-ball.radius < 0 || int(ball.y)+ball.radius > winHeight {
+		ball.yv = -ball.yv
+	}
+	if ball.x < 0 || int(ball.x) > winWidth {
+		ball.x = 300
+		ball.y = 300
+	}
+
+	if int(ball.x) < int(leftPaddle.x)+leftPaddle.w/2 {
+		if int(ball.y) > int(leftPaddle.y)-leftPaddle.h/2 && int(ball.y) < int(leftPaddle.y)+leftPaddle.h/2 {
+			ball.xv = -ball.xv
+		}
+	}
+	if int(ball.x) > int(rightPaddle.x)-rightPaddle.w/2 {
+		if int(ball.y) > int(rightPaddle.y)-rightPaddle.h/2 && int(ball.y) < int(rightPaddle.y)+rightPaddle.h/2 {
+			ball.xv = -ball.xv
+		}
+	}
+}
+
+//PADDLE STUFF
 type paddle struct {
 	pos
 	w     int
@@ -50,6 +78,26 @@ func (paddle *paddle) draw(pixels []byte) {
 		for x := 0; x < paddle.w; x++ {
 			setPixel(startX+x, startY+y, paddle.color, pixels)
 		}
+	}
+}
+
+//Player 2 (computer)
+func (paddle *paddle) aiUpdate(ball *ball) {
+	paddle.y = ball.y
+}
+
+//Paddle movement
+func (paddle *paddle) update(keyState []uint8) {
+	if keyState[sdl.SCANCODE_UP] != 0 {
+		paddle.y--
+	}
+	if keyState[sdl.SCANCODE_DOWN] != 0 {
+		paddle.y++
+	}
+}
+func clear(pixels []byte) {
+	for i := range pixels {
+		pixels[i] = 0
 	}
 }
 
@@ -99,8 +147,12 @@ func main() {
 
 	pixels := make([]byte, winWidth*winHeight*4)
 
-	player1 := paddle{pos{100, 100}, 20, 100, color{255, 255, 255}}
-	ball := ball{pos{300, 300}, 20, 0, 0, color{255, 255, 255}}
+	player1 := paddle{pos{50, 100}, 20, 100, color{255, 255, 255}}
+	player2 := paddle{pos{float32(winWidth) - 50, 100}, 20, 100, color{255, 255, 255}}
+
+	ball := ball{pos{300, 300}, 20, .2, .2, color{255, 255, 255}}
+	keyState := sdl.GetKeyboardState()
+
 	//Big game loop
 	for {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
@@ -110,14 +162,20 @@ func main() {
 			}
 		}
 		//game
+		clear(pixels)
+		player1.update(keyState)
+		player2.aiUpdate(&ball)
+		ball.update(&player1, &player2)
+
 		player1.draw(pixels)
+		player2.draw(pixels)
 		ball.draw(pixels)
 
 		//Render on the screen
-		tex.Update(nil, pixels, winWidth*4)
-		renderer.Copy(tex, nil, nil)
+		_ = tex.Update(nil, pixels, winWidth*4)
+		_ = renderer.Copy(tex, nil, nil)
 		renderer.Present()
-		sdl.Delay(2000)
+		sdl.Delay(16)
 	}
 
 }
